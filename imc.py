6eea16ff187f876e1643464cbfb694391c4361e0
Gabriela Pontes <40066544+gabifpontes@users.noreply.github.com>
2021-05-06 20:29:56 -0300

    Update imc.py
    Código corrigido

--- imc.py
+++ imc.py
@@ -2,25 +2,25 @@
 
 def calculo_imc(peso, altura):
   imc = peso / (altura ** 2)
-return imc
+  return imc
 
 def classificacao(imc):
     if imc < 18.5:
-      print("Baixo Peso")
+      return "Baixo Peso"
     elif imc >= 18.5 and imc < 25:
-      print("Peso Normal")    
+      return "Peso Normal"    
     elif imc >= 25 and imc < 30:
-      print("Sobrepeso")    
+      return "Sobrepeso"    
     elif imc >= 30 and imc < 35:
-      print("Obesidade Grau I")    
+      return "Obesidade Grau I"    
     elif imc >= 35 and imc < 40:
-      print("Obesidade Severa (Grau II)")    
+      return "Obesidade Severa (Grau II)"    
     else:
-      print("Obesidade Mórbida (Grau III)")
+      return "Obesidade Mórbida (Grau III)"
 
 peso = float(input("Qual é o seu peso (kg)? "))
 altura = float(input("Qual é a sua altura (m²)? "))
 
 meu_imc = calculo_imc(peso, altura)
 
-print('Seu IMC é ' imc 'e sua condição é ' classificacao)
+print('Seu IMC é ', meu_imc, 'e sua condição é ', classificacao(meu_imc))
